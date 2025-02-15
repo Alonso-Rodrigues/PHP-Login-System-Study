ff4b42be1284b1f6f2894b3a3ef984d7928be4f5
Alonso <alonso.rbass@gmail.com>
2025-02-15 21:34:16 +0100

    feat: transform into PHP file

--- public/login.php
+++ public/login.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./assets/css/geral.css">
 </head>
 
 <body>
@@ -13,7 +13,7 @@
     <section>
       <div class="login">
         <h1>Login Sytem Study</h1>
-        <input type="text" placeholder="name">
+        <input type="email" placeholder="email">
         <input type="password" placeholder="password">
         <button>Send</button>
       </div>
