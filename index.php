8f865b85ddc817facf45c8c4ec1e57acb0ff1644
Alonso <alonso.rbass@gmail.com>
2025-02-17 13:23:21 +0100

    feat: update CSS and create geral CSS

--- index.php
+++ index.php
@@ -5,23 +5,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./public/assets/css/index.css">
+  <link rel="stylesheet" href="./public/assets/css/geral.css">
   <title>Home</title>
 </head>
 
 <body>
-  <section class="menu_container">
+  <header class="menu_container">
     <nav class="menu_items">
       <ul>
-        <li><a href="./index.html">Home</a></li>
+        <li><a href="./">Home</a></li>
         <li><a href="/public/pages/articles.php">Articles</a></li>
         <li><a href="/public/pages/register.php">Register</a></li>
       </ul>
       <button><a href="/public/pages/login.php">Login</a></button>
     </nav>
-  </section>
+  </header>
   <main>
     <section class="principal">
-      <h1>Welcome</h1>  
+      <h1>Welcome</h1>
+    </section>
+    <section>
+      <h1>Welcome</h1>
+    </section>
+    <section>
+      <h1>Welcome</h1>
     </section>
   </main>
   <footer>
